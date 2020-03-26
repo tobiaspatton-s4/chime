79f2185746b9ee2da698014120c32f13d0cb6fe7
Tobias Patton <tobias.patton@semaphoresolutions.ca>
2020-03-26 09:07:37 -0700

    Adding readme to API

--- src/api.py
+++ src/api.py
@@ -12,6 +12,30 @@ from penn_chime.utils import RateLos
 
 application = Flask(__name__)
 
+@application.route('/api/readme', methods=['GET'])
+def get_readme():
+    return """
+# Chime API
+
+This API wraps the modeling engine that backs the [COVID-19 Hospital Impact Model for Epidemics tool](https://codeforphilly.github.io/chime/)
+
+## Usage
+
+When following these instructions replace <host> with the actual host. Currently this is "dry-basin-64387.herokuapp.com".
+
+To see these instructions: `curl https://<host>/api/readme`
+
+To retrieve a JSON file that contains the required arguments:
+
+`curl https://<host>/api/args > args.json`
+
+Modify the args file with your parameters, then post the file to the API:
+
+`curl -X POST -d @args.json -H "content-type: application/json"  https://<host>/api/args --output results.zip && unzip results.zip`
+
+Note that this is a quick and dirty wrapper and should not be thought of as production-quality code. For example, no validation is done on inputs. If you get a server error when POSTing some parameters, make a change and try again.
+"""
+
 @application.route('/api/args', methods=['GET'])
 def get_args():
     return application.send_static_file('sample_args.json')
